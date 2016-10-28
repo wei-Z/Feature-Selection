732028cf5435e05b5feae81249980bc71112bb35
wei-Z <52lynn25@gmail.com>
2016-10-28 18:50:17 -0400

    first commit

---
+++ featureSelection.py
@@ -0,0 +1,62 @@
+#Feature Selection in Python
+
+# 1 Univariate Selection
+## Feature Extraction with Univariate Statistical Tests (Chi-squared for classification)
+import pandas as pd
+import numpy as np
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+# load data
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pd.read_csv(url, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+# feature extraction
+test = SelectKBest(score_func=chi2, k=4)
+fit = test.fit(X, Y)
+# summarize scores
+np.set_printoptions(precision=3)
+print fit.scores_
+features = fit.transform(X)
+# summarize selected features
+print features[0:5,:]
+
+# 2 Recursive Feature Elimination
+## Feature Extraction with RFE
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+# load data
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pd.read_csv(url, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+# feature extraction
+model = LogisticRegression()
+rfe = RFE(model, 3)
+fit = rfe.fit(X, Y)
+print "Num Features: %d" % fit.n_features_
+print "Selected Features: %s" % fit.support_
+print "Feature Ranking: %s" % fit.ranking_
+
+# 3 Principal Component Anlysis 
+## Feature Extraction with PCA
+from sklearn.decomposition import PCA
+# load data
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pd.read_csv(url, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+# feature extraction
+pca = PCA(n_components=3)
+fit = pca.fit(X)
+# summarize components
+print "Explained Variance: %s" % fit.explained_variance_ratio_
+print fit.components_
+
+
